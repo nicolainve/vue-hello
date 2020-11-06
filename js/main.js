92e2e4a84949a2850b4d8024bf867e54afd20b2a
Nicola <nginvernizzi1@gmail.com>
2020-11-06 15:29:28 +0100

    js done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log(Vue);
+const app = new Vue ({
+    el: '#root',
+    data: {
+        message: 'Hello World from Vue.js',
+        image: 'https://miro.medium.com/max/500/0*xkJgg-6HskYrQ3N7.jpeg',
+        style: 'text-align: center; margin: 30px'
+    }
+})
